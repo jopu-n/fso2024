adb1816b393c4d686eb22cb20f9b9a5480cf9a31
Johannes <johannesnatunen@gmail.com>
2024-07-25 02:08:41 +0300

    osa 1c

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,27 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>
-        Hello {props.name} you are {props.age} years old
-      </p>
-    </div>
-  );
+import { useState } from "react";
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
 };
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
-      <h1> Greetings </h1>
-      <Hello name="Jope" age={25} />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   );
 };
