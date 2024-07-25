c84fc33bbe2f50f030b4bcbe228d53d31ec44e89
Johannes <johannesnatunen@gmail.com>
2024-07-25 15:59:17 +0300

    osa1d alkuosa

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,45 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
+const History = ({ allClicks }) => {
+  return allClicks.length === 0 ? (
+    <div>the app is used by pressing the buttons</div>
+  ) : (
+    <div>button press history: {allClicks.join(" ")}</div>
+  );
 };
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
 
-  const setToZero = () => setCounter(0);
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
     </div>
   );
 };
