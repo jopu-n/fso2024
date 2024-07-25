d659d19ed29fe677e7aaf50447c8dcc8785253b5
Johannes <johannesnatunen@gmail.com>
2024-07-25 16:30:17 +0300

    osa1d loppuosan jutut

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,45 +1,25 @@
 import { useState } from "react";
 
-const History = ({ allClicks }) => {
-  return allClicks.length === 0 ? (
-    <div>the app is used by pressing the buttons</div>
-  ) : (
-    <div>button press history: {allClicks.join(" ")}</div>
-  );
-};
+const Display = (props) => <div>{props.value}</div>;
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
 );
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
-      <p>total {total}</p>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
