9dad56eb421a626fc09ca63dcfb9b42130f34b13
Johannes <johannesnatunen@gmail.com>
2024-07-30 03:01:56 +0300

    osa2.6 - 2.10

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+
+const Contact = ({ person }) => {
+  return (
+    <div>
+      <p>
+        {person.name} {person.number}
+      </p>
+    </div>
+  );
+};
+
+const TextFilter = ({ filterText, handleFilterText }) => {
+  return (
+    <div>
+      filter contacts: <input value={filterText} onChange={handleFilterText} />
+    </div>
+  );
+};
+
+const AddForm = ({
+  handleAddPerson,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange,
+}) => {
+  return (
+    <form onSubmit={handleAddPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Contacts = ({ filterText, persons }) => {
+  return (
+    <div>
+      {filterText.trim() === ""
+        ? persons.map((person) => <Contact key={person.name} person={person} />)
+        : persons
+            .filter((person) =>
+              person.name
+                .toLowerCase()
+                .includes(filterText.trim().toLowerCase())
+            )
+            .map((person) => <Contact key={person.name} person={person} />)}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterText, setFilterText] = useState("");
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    if (newName.trim() === "" || newNumber.trim() === "") {
+      return alert("Name and number required");
+    }
+
+    let nameIsInList = false;
+    let numberIsInList = false;
+    persons.forEach((person) => {
+      if (person.name === newName) {
+        nameIsInList = true;
+      }
+      if (person.number === newNumber) {
+        numberIsInList = true;
+      }
+    });
+    if (nameIsInList || numberIsInList) {
+      setNewName("");
+      setNewNumber("");
+      return nameIsInList
+        ? alert(`${newName} is already in contacts`)
+        : alert(`${newNumber} is already in contacts`);
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterText = (event) => setFilterText(event.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <TextFilter filterText={filterText} handleFilterText={handleFilterText} />
+      <h2>Add a new</h2>
+      <AddForm
+        handleAddPerson={handleAddPerson}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <h2>Numbers</h2>
+      <Contacts filterText={filterText} persons={persons} />
+    </div>
+  );
+};
+
+export default App;
